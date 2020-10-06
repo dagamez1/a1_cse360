5b98a65d9fc2c36a6213a189caea6c015b170906
danie <danie@192.168.0.4>
2020-10-05 22:46:17 -0700

    Initial commit of project.

---
+++ a1_cse360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,62 @@
+package cse360assignment02;
+/**
+ * 
+ * @author danie
+ *
+ */
+public class AddingMachine 
+{
+	/**
+	 * This program takes in integers and adds or subtracts from a total. 
+	 */
+	private int total;
+	
+	public AddingMachine () 
+	{
+		/**
+		 * Creates an object for the class AddingMachine with a total = 0 by default
+		 */
+		total = 0; // not needed - included for clarity
+	}
+	
+	public int getTotal ()
+	{
+		/**
+		 * Return the integer value representing the total in the machine 
+		 */
+		return 0;
+	}
+	
+	public void add (int value)
+	{
+		/**
+		 * This method is used to add the integer value into the total in the machine.
+		 * @param value This is the value of the integer being added to the total.
+		 */
+	}
+	
+	public void subtract (int value)
+	{
+		/**
+		 * This method is used to subtract the integer value from the total in the machine.
+		 * @param value This is the value of the integer being subtracted from the total.
+		 */
+	}
+	
+	public String toString ()
+	{
+		/**
+		 * This method is used to return a String object representing the value of a number object 
+		 */
+		return "";
+	}
+	
+	public void clear()
+	{
+		/**
+		 * This method is to clear the total in the machine. 
+		 */
+	}
+}
+	
+	
