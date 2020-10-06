b6411aad062f3b851be9075689fb2c193cb0c872
danie <danie@192.168.0.4>
2020-10-05 23:58:05 -0700

    This is the updated file of AddingMachine.

--- a1_cse360/src/cse360assignment02/AddingMachine.java
+++ a1_cse360/src/cse360assignment02/AddingMachine.java
@@ -1,4 +1,5 @@
 package cse360assignment02;
+import java.util.*;
 /**
  * 
  * @author danie
@@ -10,21 +11,28 @@ public class AddingMachine
 	 * This program takes in integers and adds or subtracts from a total. 
 	 */
 	private int total;
+	private int[] input;
 	
 	public AddingMachine () 
 	{
 		/**
 		 * Creates an object for the class AddingMachine with a total = 0 by default
+		 * 
 		 */
+		
 		total = 0; // not needed - included for clarity
+		
 	}
+	AddingMachine machine = new AddingMachine();
+	
 	
 	public int getTotal ()
 	{
+		
 		/**
 		 * Return the integer value representing the total in the machine 
 		 */
-		return 0;
+		return total;
 	}
 	
 	public void add (int value)
@@ -33,6 +41,12 @@ public class AddingMachine
 		 * This method is used to add the integer value into the total in the machine.
 		 * @param value This is the value of the integer being added to the total.
 		 */
+		total = total + value;
+		.toString() = toString() "+ " + value; 
+		
+		
+		
+		
 	}
 	
 	public void subtract (int value)
@@ -41,6 +55,9 @@ public class AddingMachine
 		 * This method is used to subtract the integer value from the total in the machine.
 		 * @param value This is the value of the integer being subtracted from the total.
 		 */
+		total = total - value;
+		toString() = toString + "- " + value;
+		
 	}
 	
 	public String toString ()
@@ -48,11 +65,14 @@ public class AddingMachine
 		/**
 		 * This method is used to return a String object representing the value of a number object 
 		 */
-		return "";
+		
+		return "0 ";
 	}
 	
 	public void clear()
 	{
+		total = 0;
+		
 		/**
 		 * This method is to clear the total in the machine. 
 		 */
